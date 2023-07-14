6d843dfbe8358f9f1df54d1dad94bdb83713dd9c
yermukhanJJ <ermuhannurkali@gmail.com>
2023-07-14 20:47:30 +0600

    add caching

--- src/main/java/kz/silk/onlinebank/service/impl/RoleServiceImpl.java
+++ src/main/java/kz/silk/onlinebank/service/impl/RoleServiceImpl.java
@@ -4,8 +4,11 @@ import kz.silk.onlinebank.domain.dao.RoleRepository;
 import kz.silk.onlinebank.domain.exceptions.NotFoundException;
 import kz.silk.onlinebank.domain.model.Role;
 import kz.silk.onlinebank.service.RoleService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,8 +19,9 @@ public class RoleServiceImpl implements RoleService {
     public static final String SERVICE_VALUE = "RoleServiceImpl";
     private final RoleRepository roleRepository;
 
+    @Cacheable(cacheNames = "role", key = "#title")
     @Override
-    public Role getRoleByTitle(String title) throws NotFoundException {
+    public Role getRoleByTitle(@NonNull String title) throws NotFoundException {
         return roleRepository.findByTitle(title)
                 .orElseThrow(() -> new NotFoundException(String.format("Role with title %s not found", title)));
     }
